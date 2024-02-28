81e3f9a6ffb11ef7ae3b9fba081f3b238c323f53
victor barbier <victor.barbier@recherche.gouv.fr>
2024-02-28 15:11:32 +0100

    feat(networks): test templates

--- client/src/pages/networks/config.tsx
+++ client/src/pages/networks/config.tsx
@@ -5,6 +5,7 @@ const terminologyAuthors = {
   links: "co-authors",
   cluster: "community",
   clusters: "communities",
+  link_strength: "co-publications",
   total_link_strength: "publications",
 }
 const terminologyInstitutions = {
@@ -14,6 +15,7 @@ const terminologyInstitutions = {
   links: "co-institutions",
   cluster: "community",
   clusters: "communities",
+  link_strength: "co-publications",
   total_link_strength: "publications",
 }
 const terminologyStructures = {
@@ -23,6 +25,7 @@ const terminologyStructures = {
   links: "co-structures",
   cluster: "community",
   clusters: "communities",
+  link_strength: "co-publications",
   total_link_strength: "publications",
 }
 const terminologyDomains = {
@@ -32,6 +35,7 @@ const terminologyDomains = {
   links: "co-domains",
   cluster: "community",
   clusters: "communities",
+  link_strength: "co-publications",
   total_link_strength: "publications",
 }
 
@@ -42,9 +46,16 @@ const TERMINOLOGY_MAPPING = {
   domains: terminologyDomains,
 }
 
+const templates = (networkTab: string) => ({
+  // prettier-ignore
+  item_description: `<div class='description_label'><a class='description_url' href=http://localhost:4173/${(networkTab === "authors" ? networkTab : "organizations")}/{id} target='_self'>{label}</a></div><div class='description_text'>Ut enim ad minim veniam, quis nostrud exercitation</div>`,
+  link_description: `<div class='description_heading'>Co-${networkTab} link</div><div class='description_label'><div class='description_text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit</div>`,
+})
+
 export default function getConfig(networkTab: string) {
+  console.log("templates", templates(networkTab))
   const config = {
-    templates: {},
+    // templates: templates(networkTab),
     ...(networkTab in TERMINOLOGY_MAPPING && { terminology: TERMINOLOGY_MAPPING[networkTab] }),
   }
   return config
