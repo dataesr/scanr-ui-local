e73039704d69cb8e61c0a1b5f13150bb6429733c
victor barbier <victor.barbier@recherche.gouv.fr>
2024-03-14 09:55:58 +0100

    enh(networks): specific info if empty query

--- client/src/api/networks/info.ts
+++ client/src/api/networks/info.ts
@@ -2,7 +2,10 @@ import { NetworkInfo } from "../../types/network"
 
 export default function infoCreate(query: string, model: string): NetworkInfo {
   const title = `Co-${model} network `
-  const description = `This is a visualization of a ${model} network from scientific publications related to "${query}"`
+  const description =
+    query === "*"
+      ? `This is a visualization of a ${model} network from all scientific publications.`
+      : `This is a visualization of a ${model} network from scientific publications related to "${query}".`
 
   return { title: title, description: description }
 }
