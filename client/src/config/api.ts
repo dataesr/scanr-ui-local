2f8a883966433bd0667acbe5a84a9b2c0485b8cf
Mihoub Debache <debache.mihoub@gmail.com>
2024-04-04 13:18:03 +0200

    fix(patents): change patents index name

--- client/src/config/api.ts
+++ client/src/config/api.ts
@@ -13,6 +13,6 @@ export const publicationsIndex = `${API_URL}/scanr-publications`;
 export const authorsIndex = `${API_URL}/scanr-persons`;
 export const organizationsIndex = `${API_URL}/scanr-organizations`;
 export const projectsIndex = `${API_URL}/scanr-projects`;
-export const patentsIndex = `${API_URL}/scanr-patents-20240403`;
+export const patentsIndex = `${API_URL}/scanr-patents`;
 export const localisationIndex = `${API_URL}/scanr-localisations`;
 export const topicsURL = TOPICS_URL ? `${TOPICS_URL}/topics` : "/topics";
