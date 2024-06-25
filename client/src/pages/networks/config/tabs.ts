7d27b9c0f3cd88ae312ca60d3cb138448a0c7f38
victor barbier <victor.barbier@recherche.gouv.fr>
2024-06-25 11:49:18 +0200

    feat(networks): add separate config

---
+++ client/src/pages/networks/config/tabs.ts
@@ -0,0 +1,31 @@
+const NETWORK_TABS_MAPPING = {
+  domains: {
+    index: 0,
+    label: "domains",
+    icon: "book-2-line",
+  },
+  authors: {
+    index: 1,
+    label: "authors",
+    icon: "user-line",
+  },
+  institutions: {
+    index: 2,
+    label: "institutions",
+    icon: "building-line",
+  },
+  structures: {
+    index: 3,
+    label: "structures",
+    icon: "microscope-line",
+  },
+  software: {
+    index: 4,
+    label: "software",
+    icon: "terminal-box-line",
+  },
+}
+
+export const networkTabs = Object.values(NETWORK_TABS_MAPPING).sort((a, b) => a.index - b.index)
+export const networkTabFindIndex = (label: string) => networkTabs.findIndex((tab) => tab.label === label)
+export const networkTabFindLabel = (index: number) => networkTabs[index].label
