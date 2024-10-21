a34a0d08448f240d485f137ef174ef1594f8b602
victor barbier <victor.barbier@recherche.gouv.fr>
2024-10-21 11:26:16 +0200

    fix(networks): nodes infos

--- client/src/api/networks/network/communities.ts
+++ client/src/api/networks/network/communities.ts
@@ -87,7 +87,7 @@ const communityGetNodesInfos = (hits: ElasticHits, model: string): any =>
   hits.reduce((acc, hit) => {
     const field = CONFIG[model].field.split(".")[0]
     const citationsByYear = hit?.["cited_by_counts_by_year"]
-    hit?.[field].forEach((node) => {
+    hit?.[field]?.forEach((node) => {
       const key = node[CONFIG[model].field.split(".")[1]]
       if (!key) return
       const id = nodeGetId(key)
