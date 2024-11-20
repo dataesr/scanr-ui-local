913881578e241b99a025509db5739ba9c9ef9a68
victor barbier <victor.barbier@recherche.gouv.fr>
2024-11-20 17:09:20 +0100

    feat(networks): dark theme

--- client/src/pages/networks/components/graph/default.tsx
+++ client/src/pages/networks/components/graph/default.tsx
@@ -1,11 +1,19 @@
+import { useIntl } from "react-intl"
 import { Container, Text } from "@dataesr/dsfr-plus"
 import NetworksSearchBar from "../search-bar"
 
 export default function NetworkGraphDefault() {
+  const intl = useIntl()
+  const theme = document.documentElement.getAttribute("data-fr-theme")
+
   return (
-    <Container fluid style={{ width: "80%", backgroundColor: "white" }}>
-      <Text>Commencez à explorer les réseaux de la recherche et de l'innovation</Text>
-      <NetworksSearchBar isLarge />
+    <Container fluid className={`graph-background-${theme}`} style={{ display: "flex", width: "85%", height: "85%" }}>
+      <Container className={`graph-title-${theme}`} style={{ alignSelf: "center" }}>
+        <Text className="fr-mb-1w" size="lg">
+          {intl.formatMessage({ id: "networks.search-bar.label-graph" })}
+        </Text>
+        <NetworksSearchBar isLarge />
+      </Container>
     </Container>
   )
 }
