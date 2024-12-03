052fed4472cf8c39deda6d3210c001053af63cb9
victor barbier <victor.barbier@recherche.gouv.fr>
2024-12-03 12:29:53 +0100

    feat(trends): uix

--- client/src/pages/trends/components/panel/header/index.tsx
+++ client/src/pages/trends/components/panel/header/index.tsx
@@ -1,23 +1,52 @@
-import { Col, Row, Text } from "@dataesr/dsfr-plus"
-import { TRENDS_VIEWS_LABELS } from "../../../config/views"
-import TrendsViewButton from "../button"
+import { Button, Col, Row, Text } from "@dataesr/dsfr-plus"
 import { useIntl } from "react-intl"
+import { useTrendsContext } from "../../../context"
+import { trendsViewFromLabel, trendsViewGetConfig } from "../../../config/views"
+
+function TrendsViewButton({ label }) {
+  const intl = useIntl()
+  const { view, setView, setFocus } = useTrendsContext()
+  const viewConfig = trendsViewGetConfig(view)
+  const defaultView = trendsViewFromLabel(label)
+
+  const isSelected = Boolean(label === viewConfig.label)
+  const onButtonClick = () => {
+    isSelected ? viewConfig?.nextView && setView(viewConfig.nextView) : setView(defaultView)
+    setFocus("")
+  }
+
+  return (
+    <Button
+      style={{ width: "100%" }}
+      icon={isSelected ? (viewConfig.order == "top" ? "arrow-up-line" : "arrow-down-line") : ""}
+      iconPosition="right"
+      variant={isSelected ? "tertiary" : "text"}
+      onClick={onButtonClick}
+    >
+      {intl.formatMessage({ id: `trends.views.header.${label}` })}
+    </Button>
+  )
+}
 
 export default function TrendsViewHeader() {
   const intl = useIntl()
 
   return (
-    <Row verticalAlign="middle">
-      <Col lg="3">
-        <Text className="fr-mb-0">{intl.formatMessage({ id: `trends.views.header.domains` })}</Text>
+    <Row verticalAlign="middle" horizontalAlign="center">
+      <Col lg="4">
+        <Text style={{ justifySelf: "center" }} className="fr-mb-0">
+          {intl.formatMessage({ id: `trends.views.header.domains` })}
+        </Text>
       </Col>
-      {TRENDS_VIEWS_LABELS.map((label, index) => (
-        <Col key={index} lg="3">
-          <Row key={index} horizontalAlign="right">
-            <TrendsViewButton key={index} label={label} />
-          </Row>
-        </Col>
-      ))}
+      <Col lg="2">
+        <TrendsViewButton label="count" />
+      </Col>
+      <Col lg="3">
+        <TrendsViewButton label="diff" />
+      </Col>
+      <Col lg="3">
+        <TrendsViewButton label="trend" />
+      </Col>
     </Row>
   )
 }
--- client/src/pages/trends/components/panel/item/index.tsx
+++ client/src/pages/trends/components/panel/item/index.tsx
@@ -16,34 +16,32 @@ export default function TrendsViewItem({ item }) {
 
   return (
     <Container fluid>
-      <Row key={item.label}>
-        <Col lg="3">
-          <Row verticalAlign="middle">
-            <Button
-              key={item.label}
-              variant={isFocused ? "tertiary" : "text"}
-              onClick={() => setFocus(isFocused ? "" : item.label)}
-            >
-              {item.label}
-            </Button>
-            {isFocused && <Wikidata item={item} />}
-          </Row>
+      <Row verticalAlign="middle">
+        <Col lg="4">
+          <Button
+            style={{ width: "100%", justifyContent: "center" }}
+            variant={isFocused ? "tertiary" : "text"}
+            onClick={() => setFocus(isFocused ? "" : item.label)}
+          >
+            {item.label}
+          </Button>
+          {isFocused && <Wikidata item={item} />}
         </Col>
-        <Col lg="3">
+        <Col lg="2">
           <Row horizontalAlign="right">
             <div>{item?.count?.[MAX_YEAR] || 0}</div>
           </Row>
         </Col>
         <Col lg="3">
           <Row horizontalAlign="right">
-            <Badge noIcon key={`diff-${item.label}`} color={diffColor}>
+            <Badge noIcon color={diffColor}>
               {`${Number(item.diff * 100).toFixed(0.1)} %`}
             </Badge>
           </Row>
         </Col>
         <Col lg="3">
           <Row horizontalAlign="right">
-            <Badge noIcon key={`trend-${item.label}`} color={slopeColor}>
+            <Badge noIcon color={slopeColor}>
               {trendState}
             </Badge>
           </Row>
