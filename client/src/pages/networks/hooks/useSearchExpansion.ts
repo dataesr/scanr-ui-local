18cfad4dae105e4397d5861256ba6c7e3346b992
victor barbier <victor.barbier@recherche.gouv.fr>
2024-12-16 12:26:02 +0100

    feat(networks): mistral search bar suggestions

--- client/src/pages/networks/hooks/useSearchExpansion.ts
+++ client/src/pages/networks/hooks/useSearchExpansion.ts
@@ -3,17 +3,18 @@ import useUrl from "../../search/hooks/useUrl"
 import { mistralAgentCompletion } from "../../../api/networks/network/mistralai"
 import { useMemo } from "react"
 
-export default function useSearchExpansion() {
+export default function useSearchExpansion(forcedQuery?: string) {
   const { currentQuery } = useUrl()
+  const query = forcedQuery || currentQuery
 
   const { data, error, isFetching } = useQuery({
-    queryKey: ["network", "expansion", currentQuery],
+    queryKey: ["network", "expansion", query],
     queryFn: () =>
       mistralAgentCompletion({
-        query: currentQuery,
+        query: query,
         agentId: "ag:03ee88d7:20241211:extend-search:042576a9",
       }),
-    enabled: currentQuery?.length > 3,
+    enabled: query?.length > 3,
   })
 
   const values = useMemo(() => {
