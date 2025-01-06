21776b1752e469375b691dacff37e076969c970c
victor barbier <victor.barbier@recherche.gouv.fr>
2025-01-06 14:58:38 +0100

    feat(trends): integration showTrendsOnly

--- client/src/pages/trends/layout/index.tsx
+++ client/src/pages/trends/layout/index.tsx
@@ -3,8 +3,13 @@ import TrendsHeader from "../components/header"
 import TrendsView from "../components/panel"
 import TrendsOptionsBar from "../components/options-bar"
 import TrendsOptionsModals from "../components/options-bar/modals"
+import useIntegration from "../hooks/useIntegration"
 
 export default function TrendsLayout() {
+  const { integrationOptions } = useIntegration()
+
+  if (integrationOptions?.showTrendsOnly) return <TrendsView />
+
   return (
     <Container fluid>
       <TrendsHeader />
