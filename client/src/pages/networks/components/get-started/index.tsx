d5aade2291e30873fcd8fe8cbc151efaff27fb3b
victor barbier <victor.barbier@recherche.gouv.fr>
2025-01-06 15:17:32 +0100

    feat(networks): disable get-started for select-source until other source dev

--- client/src/pages/networks/components/get-started/index.tsx
+++ client/src/pages/networks/components/get-started/index.tsx
@@ -2,7 +2,6 @@ import { Button, Container, Row, Text, useDSFRConfig } from "@dataesr/dsfr-plus"
 import { NetworkContext } from "../../context"
 import NetworkSearchBarGetStarted from "../search-bar/get-started"
 import NetworkSelectModelGetStarted from "../select-model/get-started"
-import NetworkSelectSourceGetStarted from "../select-source/get-started"
 import NetworksHeader from "../header"
 import { RawIntlProvider, createIntl, useIntl } from "react-intl"
 import { messages } from "../../config/messages"
@@ -54,9 +53,9 @@ function NetworkGetStartedPages() {
       return <NetworkGetStartedHome />
     case "1":
       return <NetworkSearchBarGetStarted />
+    // case "2":
+    //   return <NetworkSelectSourceGetStarted />
     case "2":
-      return <NetworkSelectSourceGetStarted />
-    case "3":
       return <NetworkSelectModelGetStarted />
     default:
       return <NetworkGetStartedHome />
