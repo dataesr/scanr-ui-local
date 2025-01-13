e0ede785d07f89272a1768770bccd728e7889abc
victor barbier <victor.barbier@recherche.gouv.fr>
2025-01-13 09:32:06 +0100

    feat(studio): locale

--- client/src/pages/studio/components/tabs/define/index.tsx
+++ client/src/pages/studio/components/tabs/define/index.tsx
@@ -7,14 +7,23 @@ export default function StudioDefine() {
   return (
     <Container>
       <Text>{intl.formatMessage({ id: "studio.define.text" })}</Text>
-      <Link href="https://barometredelascienceouverte.esr.gouv.fr/declinaisons/comment-realiser-bso-local?expanded=0" target="_blank">
+      <Link
+        href="https://barometredelascienceouverte.esr.gouv.fr/declinaisons/comment-realiser-bso-local?expanded=0"
+        target="_blank"
+      >
         {intl.formatMessage({ id: "studio.define.link.bso-local" })}
       </Link>
       <br />
       <br />
-      <Link href="https://barometredelascienceouverte.esr.gouv.fr/declinaisons/comment-realiser-bso-local?expanded=1" target="_blank">
+      <Link
+        href="https://barometredelascienceouverte.esr.gouv.fr/declinaisons/comment-realiser-bso-local?expanded=1"
+        target="_blank"
+      >
         {intl.formatMessage({ id: "studio.define.link.bso-studio" })}
       </Link>
+      <br />
+      <br />
+      <Text>{intl.formatMessage({ id: "studio.define.text2" })}</Text>
     </Container>
   )
 }
