e693d81d409ba2ce9f2a88469e654d83da76531a
victor barbier <victor.barbier@recherche.gouv.fr>
2025-01-29 10:39:10 +0100

    feat(networks): add patents organizations filter

--- client/src/pages/networks/components/filters/patents.tsx
+++ client/src/pages/networks/components/filters/patents.tsx
@@ -5,6 +5,7 @@ import useUrl from "../../../search/hooks/useUrl"
 import Modal from "../../../../components/modal"
 import PatentYearFilter from "../../../search/components/patents/filters/years"
 import PatentRegionFilter from "../../../search/components/patents/filters/region-switch"
+import PatentsOrganizationsFilter from "../../../search/components/patents/filters/organizations"
 
 export default function NetworkFiltersPatentsModal() {
   const intl = useIntl()
@@ -21,10 +22,11 @@ export default function NetworkFiltersPatentsModal() {
         <Container fluid className="fr-my-2w">
           <PatentYearFilter />
           <hr className="fr-mt-3w" />
+          <PatentsOrganizationsFilter />
         </Container>
         <Container fluid className="fr-my-2w">
-          <PatentRegionFilter />
           <hr className="fr-mt-3w" />
+          <PatentRegionFilter />
         </Container>
         <div className="fr-modal__footer fr-px-0" style={{ display: "flex", width: "100%", alignItems: "center" }}>
           <div style={{ flexGrow: 1 }}>
