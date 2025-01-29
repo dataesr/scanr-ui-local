ce2224f362d37d7acb30bf58481d2d1ef6ac81bf
victor barbier <victor.barbier@recherche.gouv.fr>
2025-01-29 09:06:25 +0100

    fix(networks): add parameters to getStructureNetworkById

--- client/src/api/networks/search/organization.ts
+++ client/src/api/networks/search/organization.ts
@@ -3,7 +3,12 @@ import { networkSearch } from "./search"
 
 export async function getStructureNetworkById(id: string, model: string): Promise<Network> {
   const organizationFilter = { terms: { "affiliations.id.keyword": [id] } }
-  const data = networkSearch({ model: model, filters: [organizationFilter], lang: "fr" })
+  const data = networkSearch({
+    model: model,
+    filters: [organizationFilter],
+    lang: "fr",
+    parameters: { maxNodes: 300, maxComponents: 5, clusters: false, filterNode: "" },
+  })
 
   return data
 }
